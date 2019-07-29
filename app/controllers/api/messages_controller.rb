c30e3bcf7627da22011592c80466235170b3b6b7
tfljliar <tfljliar1101@gmail.com>
2019-07-29 17:28:41 +0900

    binding.pry削除

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    # binding.pry
     @group = Group.find(params[:group_id])
     @messages = @group.messages.includes(:user).where('id > ?', params[:id])
   end
