b21e1513be644938fdbbd0685a02e658f4097680
tfljliar <tfljliar1101@gmail.com>
2019-07-30 15:49:15 +0900

    Update 20190703050756_create_messages.rb

--- db/migrate/20190703050756_create_messages.rb
+++ db/migrate/20190703050756_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.string :content
       t.string :image
-      t.reference :group, foreign_key: true
-      t.reference :user, foreign_key: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
